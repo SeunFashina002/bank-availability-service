719c10649593ed104734947b80aaed19809a1035
Macbook <fashinaoluwaseun36@gmail.com>
2025-06-27 15:14:10 +0100

    Remove dist and .DS_Store, update .gitignore

--- dist/availability/dto/bank-availability.dto.d.ts
+++
@@ -1,8 +0,0 @@
-import { ConfidenceLevel } from '../../common/interfaces/transaction.interface';
-export declare class BankAvailabilityDto {
-    bank_code: string;
-    availability_percentage: number | null;
-    confidence_level: ConfidenceLevel;
-    time_window: string;
-    last_calculated_at: string;
-}
